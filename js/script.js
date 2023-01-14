7103788b0566baa463f02adf4f11e51f49a0c8f4
troxtele <105340164+troxtele@users.noreply.github.com>
2023-01-14 19:24:29 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,20 +1,73 @@
-
 const nav = document.querySelector("header");
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
   if (window.scrollY > 20) {
-    nav.classList.add('drop-shadow-lg')
+    nav.classList.add("drop-shadow-lg");
   } else {
     nav.classList.remove("drop-shadow-lg");
-
   }
-})
+});
+
+// Texts animation
+const heading = document.querySelector(".heading_animation");
+const pera = document.querySelector(".text_up_animation");
+const buttons = document.querySelector(".button_up_animation");
+
+// Bottle animation
+const bottle = document.querySelector(".bottle_animation");
+
+const mainFunc = () => {
+
+}
+
+const a = setTimeout(() => {
+  heading.classList.toggle("fadeInUp", "opacity-100");
+}, 500);
+
+const b = setTimeout(() => {
+  pera.classList.toggle("fadeInUp", "opacity-100");
+}, 800);
+
+const c = setTimeout(() => {
+  buttons.classList.toggle("fadeInUp", "opacity-100");
+}, 1100);
 
 
+const bottleAnimation = setTimeout(() => {
+  bottle.classList.toggle("fadeInUpBottle");
+}, 500);
+
+// fade out down
+
+const content = document.querySelector(".content_down_animation");
+
+const down = setTimeout(() => {
+  content.classList.toggle("fadeOutDown");;
+  bottle.classList.toggle("fadeOutDown");
+}, 5000);
+
+const repeatAnimation = setInterval(() => {
+  // a()
+  // b()
+  // c()
+  // bottleAnimation()
+  // down()
+  // console.log(a)
+}, 1000);
 
 
+setInterval(() => {
+  lol()
+}, 1000);
 
 
-
+// ************************ Sliders
+// Hero Slider
+var heroSwiper = new Swiper(".hero_slider", {
+  speed: 1000,
+  spaceBetween: 100,
+  slidesPerView: 1,
+  loop: true,
+});
 
 var swiper = new Swiper(".client_slider", {
   slidesPerView: 0,
@@ -34,11 +87,4 @@ var swiper = new Swiper(".client_slider", {
       spaceBetween: 50,
     },
   },
-});
-
-var heroSwiper = new Swiper(".hero_slider", {
-  speed: 1000,
-  spaceBetween: 100,
-  slidesPerView: 1,
-  loop: true,
 });
